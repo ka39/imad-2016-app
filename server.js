fa1c5ca7e1773c1bbfd1b936d5b5ac8cefa4538e
ka39 <anbumani.ka@gmail.com>
2016-10-28 13:14:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
-//web app wih connection to database
 var express = require('express');  //include express
-//var morgan = require('morgan');   
-//var path = require('path');
+var morgan = require('morgan');   
+path = require('path');
 
 var app = express();       //create application object
-//app.use(morgan('combined'));
+app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
