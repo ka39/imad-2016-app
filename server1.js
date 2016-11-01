e3c3235f5417e9d22253f3aebe47685abcc3c2e3
ka39 <anbumani.ka@gmail.com>
2016-11-01 17:21:59 +0530

    [imad-console] Updates server1.js

--- server1.js
+++ server1.js
@@ -8,7 +8,7 @@ http.createServer(function(req, res){
     //attach listener on end event of request
     req.on('end', function(){
         // write header(status & content-type) to response
-        res.writeHead(200, {Content-Type:'text/plain'});
+        res.writeHead(200, {'Content-Type':'text/plain'});
         //send body(data) and end response
         res.end('Hellow World!');
     });
